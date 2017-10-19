d8f82c20d4fb26a3e3cea4eff33393438a062f0f
Glenn Goossens <glenngoossens07@gmail.com>
2017-10-19 20:46:26 +0200

    afwerken

--- oef32.c
+++ oef32.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define aantal 1000
+#define aantal 10
+#define MAXAANTAL 6
 
 char * lees();
-char * lees_meerdere();
+char ** lees_meerdere();
 
 int main(){
   int i;
 
-  for(i=0; i<5; i++){
+  /*for(i=0; i<MAXAANTAL; i++){
     char * tekst = lees();
     printf("Ik las in %s.\n",tekst);
     free(tekst);
+  }*/
+  char ** string = lees_meerdere();
+  for(i = 0; i < MAXAANTAL;i++){
+  	if(string[i] != NULL){
+  	printf("Ingelezen = %s !\n",string[i]);
+  	free(string[i]);
   }
+  }
+  free(string);
   return 0;
 }
 
@@ -23,6 +32,15 @@ char * lees(){
   return str;
 }
 
-char * lees_meerdere(){
-
+char ** lees_meerdere(){
+	char ** strings = malloc(MAXAANTAL * sizeof(char*));
+	int i;
+	for(i = 0; i<MAXAANTAL;i++){
+		strings[i] = malloc(sizeof(char) *aantal+1);
+		fgets(strings[i],aantal,stdin);
+		if(strcmp(strings[i],"STOP")!= 1){
+			return strings;
+		}
+	}
+	return strings;
 }
