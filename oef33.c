6df60f70889b468109501a3facea56c87ceaf4b8
Glenn Goossens <glenngoossens07@gmail.com>
2017-10-25 13:40:21 +0200

    oplossing

--- oef33.c
+++ oef33.c
@@ -1,35 +1,37 @@
-#include <stdio.h>
-#include <string.h>
 
+#include <stdio.h> #include <string.h>
 #define AANTAL_WOORDEN 10
 #define GEMIDDELDE_LENGTE_WOORDEN 7
-#define LENGTE_ARRAY_T 80
-/*(gebruik AANTAL_WOORDEN en GEMIDDELDE_LENGTE_WOORDEN om dit te berekenen)
-(voorbeeld: AANTAL_WOORDEN + GEMIDDELDE_LENGTE_WOORDEN)*/
+#define LENGTE_ARRAY_T AANTAL_WOORDEN * (1+GEMIDDELDE_LENGTE_WOORDEN)
 
+/* versie met indexering van pt */ /*
 void lees(char ** pt){
- /* gebruik schuivende pointers, geen indexering */
- while(*pt != '\0'){
- 	char woord[10];
-	scanf("%s",&woord);
-	*pt = &woord[0];
-	printf("%s\n",*pt);
-	*pt++;
- }
-
+int i;
+for(i=0; i<AANTAL_WOORDEN; i++){
+scanf("%s",pt[i]);
+pt[i+1] = pt[i]+strlen(pt[i])+1; }
+pt[AANTAL_WOORDEN]=0; }*/
+/* versie met schuivende pt */
+void lees(char ** pt){
+  int i;
+  for(i=0; i<AANTAL_WOORDEN; i++){
+    scanf("%s",*pt);
+    *(pt+1) = *pt+strlen(*pt)+1;
+    pt ++;
+    }
+  *pt=0;
 }
 
-void schrijf(char ** pt){
-	
+void schrijf(char const * const * pt){
+  while(*pt!=0){
+    printf("\n%s",*pt);
+    pt ++;
+  }
 }
-
 int main(){
-char* pt[AANTAL_WOORDEN+1]; /* zodat je ook nog een nullpointer kan wegsteken op het einde van de pointertabel */
-pt[AANTAL_WOORDEN+1] = '\0';
-char t[LENGTE_ARRAY_T];
-pt[0] = t;
-printf("Geef %d woorden in:\n",AANTAL_WOORDEN);
-lees(pt); /* leest alle woorden in */
-schrijf(pt); /* schrijft alle woorden onder elkaar uit*/
-return 0;
+  char* pt[AANTAL_WOORDEN+1]; /* zodat je ook nog een nullpointer kan wegsteken op het einde van de pointertabel */
+  char t[LENGTE_ARRAY_T]; pt[0] = t;
+  printf("Geef %d woorden in:\n",AANTAL_WOORDEN); lees(pt);
+  schrijf(pt);
+  return 0;
 }
